596005f5b57745a96bb615fd8d5bf44b05c28ab3
s4 <sadegh.nobakhti@gmail.com>
2022-12-23 07:49:21 +0330

    improved

--- src/snake.c
+++ src/snake.c
@@ -14,8 +14,8 @@
 #include <fcntl.h>
 #include <termios.h>
 
-#define W       32
-#define H       16
+#define W       (32)
+#define H       (16)
 #define MAX_INX (H * W)
 #define BODY_C  's'
 #define HEAD_C  'Q'
@@ -35,125 +35,117 @@
 
 typedef struct Point {
     char c;
-    struct Point* n;  // next point
+    struct Point* next;
 } Point;
 
-// We have to store the index of the snake's head eventhough we already have
+// We have to store the index of the snake's head even though we already have
 // a pointer to it. because the `Point` does not store coordinates. We could
-// alos opt to include fields like `x` and `y` to the `Point` struct but they
+// alos opt to include fields like `x` and `y` in the `Point` struct but they
 // would have no use except for moving the head and maybe passing walls which
 // are already solved quite easily.
 typedef struct Snake {
     int len;
+    int x;
+    int y;
     int vx;   // x axis velocity
     int vy;   // y axis velocity
-    int inx;  // Snake's head inx
     Point* head;
     Point* tail;
 } Snake;
 
-void drop_apple(Point* table) {
+void drop_apple(Point table[][W]) {
     while (1) {
-        int a_inx = rand() % MAX_INX;
+        int inx = rand();
+        int y = inx % H;
+        int x = inx % W;
 
-        if (table[a_inx].c == ' ') {
-            table[a_inx].c = APPLE_C;
+        if (table[y][x].c == ' ') {
+            table[y][x].c = APPLE_C;
             break;
         }
     }
 }
 
-void init_game(Point* table, Snake* snake) {
+void init_game(Point table[][W], Snake* snake) {
     // Fill table with spaces
     for (int i = 0; i < H; ++i) {
         for (int j = 0; j < W; ++j) {
-            table[(i * W) + j].c = ' ';
-            table[(i * W) + j].n = NULL;
+            table[i][j].c = ' ';
+            table[i][j].next = NULL;
         }
     }
 
     // Fill vertical table borders with '|'
     for (int j = 0; j < W; j += (W - 1)) {
         for (int i = 0; i < H; ++i) {
-            table[(i * W) + j].c = '|';
+            table[i][j].c = '|';
         }
     }
 
     // Fill horizontal table borders with '-'
     for (int i = 0; i < H; i += (H - 1)) {
         for (int j = 0; j < W; ++j) {
-            table[(i * W) + j].c = '-';
+            table[i][j].c = '-';
         }
     }
 
     // We use a fixed position and body direction for now.
     int x = 1;
     int y = 1;
-    int inx = (y * W) + x;
 
     // Tail of the snake
-    table[inx].c = BODY_C;
-    table[inx].n = &table[inx + 1];
-    snake->tail = &table[inx];
+    table[y][x].c = BODY_C;
+    table[y][x].next = &table[y][x + 1];
+    snake->tail = &table[y][x];
 
     // Body of the snake
-    for (int i = 1; i < snake->len; ++i) {
-        table[inx + i].c = BODY_C;
-        table[inx + i].n = &table[inx + i + 1];
+    for (int j = 1; j < snake->len; ++j) {
+        table[y][j + 1].c = BODY_C;
+        table[y][j + 1].next = &table[y][x + j +1];
     }
 
     // Head of the snake
-    table[inx + snake->len].c = HEAD_C;
-    table[inx + snake->len].n = NULL;
-    snake->head = &table[inx + snake->len];
-    snake->inx = inx + snake->len;
+    table[y][x + snake->len].c = HEAD_C;
+    table[y][x + snake->len].next = NULL;
+    snake->head = &table[y][x + snake->len];
+    snake->y = y;
+    snake->x = x + snake->len;
 
     srand(time(NULL));  // Initialization, should only be called once.
     drop_apple(table);
 }
 
-void draw_table(const Point* table, const Snake* snake) {
-    cls();
-    for (int i = 0; i < H; ++i) {
-        for (int j = 0; j < W; ++j) {
-            printf("%c", table[(i * W) + j].c);
-        }
-        printf("\r\n");
-    }
-    printf("j: down, k: up, h: left, l: right\n");
-    printf("score: %d\n", snake->len - 3);
-}
-
-// Defined at the end of the file.
-int kbhit(void);
 void get_input_vxy(int* vx, int* vy) {
+    int kbhit(void); // Defined at the end of the file.
+
     char c = ' ';
     if (kbhit()) {
+        // Skip aditional characters sent by arrow keys
+        c = getchar(); if (c != 27) return;
+        c = getchar(); if (c != 91) return;
         c = getchar();
     }
-
     switch (c) {
-        case 'h':
+        case 68:      // LEFT
             *vx = -1;
             *vy = 0;
             break;
-        case 'j':
+        case 66:      // DOWN
             *vy = 1;
             *vx = 0;
             break;
-        case 'k':
+        case 65:      // UP
             *vy = -1;
             *vx = 0;
             break;
-        case 'l':
+        case 67:      // RIGHT
             *vx = 1;
             *vy = 0;
             break;
-        default:; break;
     }
 }
 
-void update_table(Point* table, Snake* snake) {
+void update_table(Point table[][W], Snake* snake) {
     int vx = 0;
     int vy = 0;
     get_input_vxy(&vx, &vy);
@@ -163,16 +155,17 @@ void update_table(Point* table, Snake* snake) {
         snake->vx = vx;
         snake->vy = vy;
     }
-    int inx = snake->inx + snake->vx + (snake->vy * W);
+    int y = snake->y + snake->vy;
+    int x = snake->x + snake->vx;
 
     // We are opting to move the tail before the head. So that the player could
     // run in a circle. Therefore we check for invalid index after moving the
     // tail.
 
-    if (table[inx].c != APPLE_C) {
+    if (table[y][x].c != APPLE_C) {
         // Move the tail one up in linked list
-        Point* temp = snake->tail->n;
-        snake->tail->n = NULL;
+        Point* temp = snake->tail->next;
+        snake->tail->next = NULL;
         snake->tail->c = ' ';
         snake->tail = temp;
     } else {
@@ -183,16 +176,16 @@ void update_table(Point* table, Snake* snake) {
 
 #if PASS_WALL == 1
     // Validate the new head position.
-    if (table[inx].c == BODY_C) {
+    if (table[y][x].c == BODY_C) {
         printf("You lost!\n");
         exit(1);
     }
 
     // Move to the other side if hitting walls.
-    if (table[inx].c == '|') {
-        inx = (inx % W) > (W / 2) ? inx - (W - 2) : inx + (W - 2);
-    } else if (table[inx].c == '-') {
-        inx = (inx / W) > (H / 2) ? inx - ((H - 2) * W) : inx + ((H - 2) * W);
+    if (table[y][x].c == '|') {
+        x = x == 0 ? x + (W -2) : x - (W - 2);
+    } else if (table[y][x].c == '-') {
+        y = y == 0 ? y + (H - 2) : y - (H - 2);
     }
 #else
     // Validate the new head position.
@@ -203,23 +196,36 @@ void update_table(Point* table, Snake* snake) {
 #endif
 
     // Set the new head position.
-    table[inx].n = NULL;
-    table[inx].c = HEAD_C;
-    snake->head->n = &table[inx];
+    table[y][x].next = NULL;
+    table[y][x].c = HEAD_C;
+    snake->head->next = &table[y][x];
     snake->head->c = BODY_C;
-    snake->head = &table[inx];
-    snake->inx = inx;
+    snake->head = &table[y][x];
+    snake->y = y;
+    snake->x = x;
 }
 
-int main(int argc, char** argv) {
-    Point table[W][H];
+void draw_table(const Point table[][W], const Snake* snake) {
+    cls();
+    for (int i = 0; i < H; ++i) {
+        for (int j = 0; j < W; ++j) {
+            printf("%c", table[i][j].c);
+        }
+        printf("\r\n");
+    }
+    printf("score: %d\n", snake->len - 3);
+}
+
+
+int main(void) {
+    Point table[H][W];
     Snake snake = {.len = 3, .vx = 1, .vy = 0};
 
-    init_game(&table[0][0], &snake);
+    init_game(table, &snake);
 
     while (1) {
-        update_table(&table[0][0], &snake);
-        draw_table(&table[0][0], &snake);
+        update_table(table, &snake);
+        draw_table(table, &snake);
 
         // loop's delay
         usleep(150000);
@@ -227,7 +233,7 @@ int main(int argc, char** argv) {
 }
 
 // Copied from the internet for non-blocking, non-line-bufferd input read Thanks
-// to Thantos's answer in this thread:
+// to Thantos for his answer in this thread:
 // https://cboard.cprogramming.com/c-programming/63166-kbhit-linux.html
 int kbhit(void) {
     struct termios oldt;
